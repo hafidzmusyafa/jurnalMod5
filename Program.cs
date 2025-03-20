90b0ccbc5cbd4c1e38e74cd60e4f17eb2fb73d60
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:50:30 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
+public class SimpleDataBase<T>
+{
+    List<T> storedData;
+    List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        foreach (T data in storedData)
+        {
+            Console.WriteLine($"Data {data} berisi: {data}, yang disimpan pada waktu UTC: {DateTime.Now}\r\n");
+        }
+    }
+}
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> dataBase = new SimpleDataBase<int>();
+
+        dataBase.AddNewData(10);
+        dataBase.AddNewData(30);
+        dataBase.AddNewData(22);
+
+        dataBase.PrintAllData();
+    }
+}
