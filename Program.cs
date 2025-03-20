b42c971b705debbe11c5e8cfa07cd5f005921129
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:20:16 +0700

    menambahkan class PemrosesanData dengan method DapatkanNilaiTersebsar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
 ﻿using System;
 
+public class PemrosesData
+{
+    public void DapatkanNilaiTerbesar<T>(T x, T y, T z)
+    {
+        dynamic X = x;
+        dynamic Y = y;
+        dynamic Z = z;
+
+        if(X > Y && X > Z)
+        {
+            Console.WriteLine("maximum: " + X);
+        } 
+        else if(Y > Z && Y > X)
+        {
+            Console.WriteLine("maximum: " + Y);
+        }
+        else if(Z > X && Z > Y)
+        {
+            Console.WriteLine("maximum: " + Z);
+        }
+    }
+}
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        PemrosesData data = new PemrosesData();
+
+        float x = 10;
+        float y = 30;
+        float z = 22;
+
+        Console.WriteLine($"{x} {y} {z}");
+        data.DapatkanNilaiTerbesar(x, y, z);
+    }
+}
--- obj/Debug/net8.0/modul5_103022300162.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_103022300162.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_103022300162")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d54963c08374e2cab79243d5c97f43d3f3f3472")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_103022300162")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_103022300162")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
